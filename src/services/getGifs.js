c6052fad5e40e10e1a2748605eb21c4af985a531
DiegoMarty <contacto@diegomarty.com>
2022-11-18 15:01:20 +0100

    ⚡️ Updated services

--- src/services/getGifs.js
+++ src/services/getGifs.js
@@ -1,22 +1,22 @@
-const apiKey = "yt0in7NMytJYOKM95bkJU3s39zvywr9C&q";
+import {API_KEY, API_URL} from './settings'
 
-export default function getGifs({ keyword = "pokemon" } = {}) {
-  const giphyRoute = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}=${keyword}&limit=25&offset=0&rating=g&lang=en`;
+const fromApiResponseToGifs = apiResponse => {
+    const {data = []} = apiResponse
+    if (Array.isArray(data)) {
+        const gifs = data.map(image => {
+            const {images, title, id} = image
+            const {url} = images.downsized_medium
+            return { title, id, url }
+        })
+        return gifs
+    }
+    return []
+}
 
-  return fetch(giphyRoute)
-    .then((res) => res.json())
-    .then((response) => {
-      const { data } = response;
+export default function getGifs({limit = 25, keyword = "pokemon" } = {}) {
+    const giphyRoute = `${API_URL}/gifs/search?api_key=${API_KEY}=${keyword}&limit=${limit}&offset=0&rating=g&lang=en`;
 
-      if (Array.isArray(data)) {
-        const gifs = data.map((image) => {
-          const { images, title, id } = image;
-          const { url } = images.downsized_medium;
-
-          return { title, id, url };
-        });
-
-        return gifs;
-      }
-    });
+    return fetch(giphyRoute)
+        .then(res => res.json())
+        .then(fromApiResponseToGifs)
 }
---
+++ src/services/settings.js
@@ -0,0 +1,2 @@
+export const API_KEY = "yt0in7NMytJYOKM95bkJU3s39zvywr9C&q";
+export const API_URL = "https://api.giphy.com/v1"
